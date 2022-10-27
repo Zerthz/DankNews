260965db1ebed9ad39fa99a5c87222aa65e92be3
Felix <56971417+Zerthz@users.noreply.github.com>
2022-10-27 14:52:51 +0200

    Assemble data to be saved, add TODO
    There's more work to be done here, when we know how our memes
    look like. For now it does the trick. More work will have to be done
    when we know how our memes look and work

---
+++ Functions/AssembleDataActivity.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Functions.Models;
+using Microsoft.DurableTask;
+
+namespace Functions
+{
+
+    // We input a tuple, because I think you can only have 1 input to this, so we then immediatly break it out of the tuple
+    // because frick tuples, all my homies hate tuples
+
+    // The 2nd tuple value should go from string to meme model
+    [DurableTask(nameof(AssembleDataActivity))]
+    public class AssembleDataActivity : TaskActivityBase<(List<News>, List<string>), List<MemeNewsModel>>
+    {
+
+        // TODO : Remove comments and add data from memes when we know what we're getting.
+        protected override List<MemeNewsModel>? OnRun(TaskActivityContext context, (List<News>, List<string>) input)
+        {
+            // break out of tuple
+            var news = input.Item1;
+            // Vet inte hur många memes det är vi kommer få? är det typ 100+ eller typ 3?
+            var memes = input.Item2;
+            // int counter = 0;
+            List<MemeNewsModel> output = new List<MemeNewsModel>();
+            foreach (var item in news)
+            {
+                // Create a new model here with the wanted data from the inputs
+                var data = new MemeNewsModel()
+                {
+                    NewsTitle = item.Title,
+                    NewsAbstract = item.Abstract,
+                    NewsSection = item.Section,
+                    NewsSubsection = item.Subsection,
+                    NewsDatePublished = item.PublishedDate,
+                    NewsByLine = item.Byline,
+                    NewsURL = item.Url,
+
+                    // Kan typ ha en counter och ta en meme från det? måste bara gardera mot range overflow
+                    //MemeURL = memes[counter]
+
+                };
+
+                // inte testat den här koden men det borde gardera mot overflows, bara loopar över memesen om och om igen. funkar för nu
+                // counter++;
+                // if (memes.Count < counter)
+                //  counter = 0;
+                output.Add(data);
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Functions/Models/MemeNewsModel.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Functions.Models
+{
+
+    // TODO : Add more meme data?
+    public class MemeNewsModel
+    {
+        public string? NewsTitle { get; set; }
+        public string? NewsAbstract { get; set; }
+        public string? NewsSection { get; set; }
+        public string? NewsSubsection { get; set; }
+        public string? NewsURL { get; set; }
+        public DateTime NewsDatePublished { get; set; }
+        public string? NewsByLine { get; set; }
+        public string? MemeURL { get; set; }
+
+    }
+}
